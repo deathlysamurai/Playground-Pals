8ad9932fa0e67b5f24a3ae61de51a3e62fe76ed7
Mathew Humphrey <deathlysamurai@live.com>
2025-05-19 18:25:56 -0500

    Create Project

---
+++ scenes/screen/menu/background_sprite.gd
@@ -0,0 +1,18 @@
+extends Sprite2D
+
+func _ready() -> void:
+	get_viewport().connect("size_changed", _on_viewport_resize)
+
+func _on_viewport_resize():
+	var viewportWidth = get_viewport().size.x
+	var viewportHeight = get_viewport().size.y
+
+	var scale_factor = viewportWidth / self.texture.get_size().x
+
+	# Optional: Center the sprite, required only if the sprite's Offset>Centered checkbox is set
+	self.set_position(Vector2(viewportWidth/2, viewportHeight/2))
+
+	# Set same scale value horizontally/vertically to maintain aspect ratio
+	# If however you don't want to maintain aspect ratio, simply set different
+	# scale along x and y
+	self.set_scale(Vector2(scale_factor, scale_factor))
