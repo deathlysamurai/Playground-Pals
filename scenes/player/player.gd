1dbf356eee048d4ba1775fceb199d31f8d21587f
Shane <enahsnalob@gmail.com>
2025-05-19 19:24:46 -0500

    added movement and jump animations

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -19,7 +19,19 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		if is_on_floor():
+			$PlayerAnimation.play("move")
+		else:
+			$PlayerAnimation.play("jump")
+		if direction < 0:
+			$PlayerAnimation.flip_h = 1
+		else:
+			$PlayerAnimation.flip_h = 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if is_on_floor():
+			$PlayerAnimation.play("idle")
+		else:
+			$PlayerAnimation.play("jump")
 
 	move_and_slide()
