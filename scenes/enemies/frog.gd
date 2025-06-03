73c6586d2e9c6567cf6c069261b7db3d276823bb
Robert Harris <haywire.hawk@gmail.com>
2025-06-03 06:14:37 -0500

    Frog movement smoothed
    Some tweaks to frog movement, adding a jumping delay.
    Added an enemy group for future

--- scenes/enemies/frog.gd
+++ scenes/enemies/frog.gd
@@ -1,7 +1,19 @@
 extends CharacterBody2D
 
-const MAX_SPEED = 50
+const MAX_SPEED = 100
 const JUMP_STRENGTH = 500
+const ACCELERATION_SMOOTHING = 20
+
+## The base delay to the frog making a jump
+@export_range(0.0, 5.0, 0.1,) var jump_delay: float = 2.0
+
+@onready var movement_timer = $MovementTimer
+
+
+func _ready() -> void:
+	movement_timer.wait_time = jump_delay
+	movement_timer.start()
+
 
 func _physics_process(delta: float) -> void:
 	# Checks if the entity has fallen too far and removes from game
@@ -11,11 +23,15 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	else: # Only jump when on ground
+	elif movement_timer.is_stopped(): # Only jump when on ground and timer stopped
 		var direction = get_direction_to_player()
 		velocity = direction * MAX_SPEED + Vector2.UP * JUMP_STRENGTH
+		movement_timer.start()
+	else:
+		velocity = velocity.lerp(Vector2.ZERO, (1 - exp(-ACCELERATION_SMOOTHING * delta)))
 	
 	move_and_slide()
+
 
 func get_direction_to_player():
 	var player_node = get_tree().get_first_node_in_group("player") as Node2D
