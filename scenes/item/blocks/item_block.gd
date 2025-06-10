df3725cfc307340a06f0f3902a55e57a3631300f
Mathew Humphrey <deathlysamurai@live.com>
2025-06-09 20:01:10 -0500

    Add Interactable Blocks

---
+++ scenes/item/blocks/item_block.gd
@@ -0,0 +1,45 @@
+extends Block
+class_name ItemBlock
+
+@export var gravity: float = 3600.0
+@export var initial_bounce_y_speed: float = -360.0
+@export var spawned_item: PackedScene
+
+@onready var bounce_timer: Timer = $BounceTimer
+
+var y_speed: float = 0.0
+var orig_y_position: float
+var hit_block_sprite: Texture
+var block_hit: bool = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	orig_y_position = sprite_2d.position.y
+	hit_block_sprite = load("res://assets/kenney_new-platformer-pack-1.0/Sprites/Tiles/Default/block_yellow.png")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if bounce_timer.is_stopped():
+		sprite_2d.position.y = orig_y_position
+		y_speed = 0.0
+	else:
+		y_speed += gravity * delta
+		sprite_2d.position.y += y_speed * delta
+
+func _bounce() -> void:
+	y_speed = initial_bounce_y_speed
+	bounce_timer.start()
+
+func _spawn_item() -> void:
+	var item = spawned_item.instantiate()
+	item.position = block_position
+	item.position.y -= sprite_2d.get_rect().size.y
+	add_child(item)
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if not block_hit:
+		_bounce()
+		_spawn_item()
+		sprite_2d.texture = hit_block_sprite
+		block_hit = true
