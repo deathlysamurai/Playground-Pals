d28b6998981f8493192f6698f83a1712f751c267
Robert Harris <haywire.hawk@gmail.com>
2025-06-14 14:35:07 -0500

    Simple Bounce on Hit
    The player will bounce back when hitting or being hit by an enemy, to provide feedback on a collision.
    
    This is a simplistic implementation, could be expanded with bounce strength modifiers, etc.

--- scenes/component/hurtbox_component.gd
+++ scenes/component/hurtbox_component.gd
@@ -3,6 +3,9 @@ class_name HurtboxComponent
 ## Component used for applying damage to a [HealthComponent] when colliding with a HitboxComponent
 ## Requires a HealthComponent in the scene
 
+## [param direction] is the direction to the hitbox causing damage.
+signal hurtbox_triggered(direction: Vector2)
+
 @export var health_component: HealthComponent
 
 
@@ -16,3 +19,7 @@ func on_area_entered(other_area: Area2D):
 	var hitbox_component = other_area as HitboxComponent
 	#print(owner.name + " hurt by " + other_area.owner.name + " for " + str(hitbox_component.damage))
 	health_component.damage(hitbox_component.damage)
+	
+	var direction = hitbox_component.global_position - self.global_position
+	hitbox_component.hit(direction)
+	hurtbox_triggered.emit(direction)
